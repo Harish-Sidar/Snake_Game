197c5b7d66814b0c06587085c2b3069f9d255b41
hk265 <Nimcet9900@gmail.com>
2025-01-30 18:20:29 +0530

    updated commit

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ const gameOverSound = new Audio('gameover.mp3');
 const moveSound = new Audio('move.mp3');
 const musicSound = new Audio('music.mp3');
 
+let score = 0;
 let speed = 5;
 let lastPaintTime = 0;
 let snakeArr = [{x:13, y:15}];
@@ -85,16 +86,71 @@ function main(ctime){
     gameEngine();
 }
 
+function isCollide(snake){
+
+         // If snake bump into yurself 
+    for (let i = 1; i < snakeArr.length; i++) {
+       
+        if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){
+            return true;
+        } 
+        // I you bump into the wall 
+        if(snake[0].x >= 18 || snake[0].x<=0 || snake[0].y >= 18 || snake[0].y<=0){
+            return true;
+        }
+        
+    }
+}
+
 function gameEngine(){
-    board.innerHTML = "";
+    // part 1 Updating the snake array and food 
+    if(isCollide(snakeArr)){
+        gameOverSound.play();
+        musicSound.pause();
+        inputDir = {x:0,y:0}; 
+        alert("Game over. Press any key to play again! ")
+        snakeArr = [{x:13, y:15}];
+        // musicSound.play();
+        score = 0;
+    }
+
+    // Skank eat a food 
+
+    if(snakeArr[0].y === food.y && snakeArr[0].x === food.x){
+        foodSound.play();
+        score += 1;
+        scoreBox.innerHTML = "Score: " + score;
+        snakeArr.unshift({x:snakeArr[0].x + inputDir.x,y:snakeArr[0].y + inputDir.y})
+        let a = 2;
+        let b = 16;
+        food = {x:Math.round(a+(b-a)*Math.random()),y:Math.round(a+(b-a)*Math.random())}
+    }
+
+    // Moving the snake 
+    for (let i = snakeArr.length -2; i >= 0; i--) {
+        snakeArr[i+1] = {...snakeArr[i]};
+      
+        
+    }
+
+    snakeArr[0].x += inputDir.x;
+    snakeArr[0].y += inputDir.y
     
-    snakeArr.forEach((e, index) => {
+
+    
+    // part 2 Display the snake and food
+    board.innerHTML = "";
+      snakeArr.forEach((e, index) => {
         let snakeElement = document.createElement('div');
         snakeElement.style.gridRowStart = e.y;
         snakeElement.style.gridColumnStart = e.x;
-        snakeElement.classList.add('snake');
+        // snakeElement.classList.add('snake');
         if (index === 0) {
             snakeElement.classList.add('head');
+        }
+        else{
+            snakeElement.classList.add('snake');
+
         }
         board.appendChild(snakeElement);
     });
@@ -107,3 +163,32 @@ function gameEngine(){
 }
 
 window.requestAnimationFrame(main);
+window.addEventListener('keydown',e =>{
+    inputDir = {x:0, y:1}// Start the game 
+    moveSound.play();
+    switch (e.key) {
+        case "ArrowUp":
+            console.log("ArrowUp")
+            inputDir.x = 0;
+            inputDir.y = -1;
+             break;
+        case "ArrowDown":
+            console.log("ArrowDown")
+            inputDir.x = 0;
+            inputDir.y = 1;
+                 break;
+         case "ArrowLeft":
+            console.log("ArrowLeft")
+            inputDir.x = -1;
+            inputDir.y = 0;
+                 break;
+         case "ArrowRight":
+             console.log("ArrowRight")
+             inputDir.x = 1;
+             inputDir.y = 0;
+                 break;
+        default:
+            break;
+    }
+    
+})
